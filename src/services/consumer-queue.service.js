e2e38c1f05c5d3ee033b9f5faee0b0ef622157d4
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-04-07 15:39:19 +0700

    received dxl

--- src/services/consumer-queue.service.js
+++ src/services/consumer-queue.service.js
@@ -5,17 +5,78 @@ const {
   consumerQueue,
 } = require('../databases/init.rabbitmq');
 
+const log = console.log;
+
+console.log = function () {
+  log.apply(console, [new Date()].concat(arguments));
+};
+
 class ConsumerQueueService {
   static async consumerToQueue(queueName) {
     try {
       const { channel, connection } = await connectToRabbitMQ();
       await consumerQueue(channel, queueName);
-      //   find user following SHOP
-      // send message
-      // yes => success
-      // error. setup DLX ...
     } catch (error) {
       console.error('Error consume message to RabbitMQ', error);
+      throw error;
+    }
+  }
+
+  //   case processing
+  static async consumerToQueueNormal(queueName) {
+    try {
+      const { channel, connection } = await connectToRabbitMQ();
+      const timeExpired = 5000;
+      setTimeout(() => {
+        channel.consume(notificationQueue, (msg) => {
+          console.log(
+            `Send notificationQueue successfully processed: `,
+            msg.content.toString()
+          );
+          channel.ack(msg);
+        });
+      }, timeExpired);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  //   case failed processing
+  static async consumerToQueueFailed(queueName) {
+    try {
+      const { channel, connection } = await connectToRabbitMQ();
+
+      const notificationExchangeDLX = 'notificationExDXL';
+      const notificationRoutingKeyDLX = 'notificationRoutingKeyDLX';
+      const notificationQueueHandler = 'notificationQueueHotFix';
+
+      await channel.assertExchange(notificationExchangeDLX, 'direct', {
+        durable: true,
+      });
+
+      const queueResult = await channel.assertQueue(notificationQueueHandler, {
+        exclusive: false,
+      });
+
+      await channel.bindQueue(
+        queueResult.queue,
+        notificationExchangeDLX,
+        notificationRoutingKeyDLX
+      );
+      await channel.consume(
+        queueResult.queue,
+        (msgFailed) => {
+          console.log(
+            `This notification was error, please hot fix: `,
+            msgFailed.content.toString()
+          );
+        },
+        {
+          noAck: true,
+        }
+      );
+    } catch (error) {
+      console.error(error);
       throw error;
     }
   }
