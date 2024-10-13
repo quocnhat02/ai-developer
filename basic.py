b91eefbd19136b1ee2883a2fb733f1b9152ba387
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-10-13 21:43:35 +0700

    function

--- basic.py
+++ basic.py
@@ -1,4 +1,22 @@
-fruits = ["banana", "lemon", "apple"]
+def sum_all(*arg):
+    print(sum(arg))
 
-for fruit in fruits:
-    print(fruit)
+
+sum_all(1, 2, 3)
+
+
+def all_info(**info):
+    for key, value in info.items():
+        print(f"{key}: {value}")
+
+
+all_info(name="Nhat", age=22)
+
+
+def fibonacci(n):
+    if n < 2:
+        return 1
+    return fibonacci(n - 2) + fibonacci(n - 1)
+
+
+print(fibonacci(5))
