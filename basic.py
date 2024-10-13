384aa8f1ebbda1aca1df6f53d8a26f6e84316968
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-10-13 21:49:28 +0700

    tuple

--- basic.py
+++ basic.py
@@ -1,22 +1,3 @@
-def sum_all(*arg):
-    print(sum(arg))
+my_tuple = [1, 2, 3, 3, "asd"]
 
-
-sum_all(1, 2, 3)
-
-
-def all_info(**info):
-    for key, value in info.items():
-        print(f"{key}: {value}")
-
-
-all_info(name="Nhat", age=22)
-
-
-def fibonacci(n):
-    if n < 2:
-        return 1
-    return fibonacci(n - 2) + fibonacci(n - 1)
-
-
-print(fibonacci(5))
+print(my_tuple)
