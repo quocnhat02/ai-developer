0e95e3ab39e6b6ce013f3687ed32ad4cc63871af
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-21 00:51:15 +0700

    beginner

---
+++ springbootmaster/src/test/java/com/beginner/springbootmaster/SpringbootmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beginner.springbootmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
