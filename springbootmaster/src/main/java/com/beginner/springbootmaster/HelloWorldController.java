348bca781b1b7857c4f578c5de677e7e6a62a76d
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-21 01:48:31 +0700

    thymeleaf

--- springbootmaster/src/main/java/com/beginner/springbootmaster/HelloWorldController.java
+++ springbootmaster/src/main/java/com/beginner/springbootmaster/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.beginner.springbootmaster;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/page")
+    public String homePages(@RequestParam(name = "userId", defaultValue = "User") String userId, Model model) {
+        model.addAttribute("userId", userId);
+        return "index";
     }
 }
--- springbootmaster/src/main/java/com/beginner/springbootmaster/MonitoringAspect.java
+++ springbootmaster/src/main/java/com/beginner/springbootmaster/MonitoringAspect.java
@@ -3,8 +3,10 @@ package com.beginner.springbootmaster;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class MonitoringAspect {
     @Before("execution(* com.beginner.springbootmaster.*.*(..))")
     public void logServiceInvocation(JoinPoint joinPoint) {
