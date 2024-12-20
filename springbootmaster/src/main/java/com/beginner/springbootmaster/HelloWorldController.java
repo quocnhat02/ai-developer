f0765551a71a2de4c16b5478643ff5b7146ab7a8
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-21 00:53:05 +0700

    controller

---
+++ springbootmaster/src/main/java/com/beginner/springbootmaster/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.beginner.springbootmaster;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
