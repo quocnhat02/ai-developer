22e5016ed06beff6e3edcf29fca950131a623f8a
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-21 23:33:21 +0700

    singleton

---
+++ springbootmaster/src/main/java/com/beginner/springbootmaster/design/creational/Singleton.java
@@ -0,0 +1,19 @@
+package com.beginner.springbootmaster.design.creational;
+
+// Đảm bảo chỉ có một thể hiện duy nhất của một lớp và
+// cung cấp một cách truy cập toàn cục tới nó.
+
+// Ex: Connection Pool, Logger, Cache, Configuration Manager
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+
+}
--- springbootmaster/src/main/java/com/beginner/springbootmaster/service/impl/BookServiceImpl.java
+++ springbootmaster/src/main/java/com/beginner/springbootmaster/service/impl/BookServiceImpl.java
@@ -6,7 +6,6 @@ import com.beginner.springbootmaster.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
