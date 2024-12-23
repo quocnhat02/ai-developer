ac9e1931b02486ad52b8b2bd2ec3a588e53ee744
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-24 00:30:17 +0700

    object mapping

--- springbootmaster/src/main/java/com/beginner/springbootmaster/controller/BookController.java
+++ springbootmaster/src/main/java/com/beginner/springbootmaster/controller/BookController.java
@@ -2,6 +2,8 @@ package com.beginner.springbootmaster.controller;
 
 import com.beginner.springbootmaster.entity.book.Book;
 import com.beginner.springbootmaster.service.BookService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,15 @@ public class BookController {
     public ResponseEntity<List<Book>> getAllBooks() {
         List<Book> books = bookService.fetchAllBooks();
         return ResponseEntity.ok(books); // Đảm bảo trả về ResponseEntity
+    }
+
+    @GetMapping("/json")
+    public ResponseEntity<Book> getJSON(ObjectMapper objectMapper) throws JsonProcessingException {
+        String bookString = "{\"id\":1,\"title\":\"Light\",\"author\":\"Nhat\"}";
+        Book book = objectMapper.readValue(bookString, Book.class);
+        System.out.println(book);
+        System.out.println(objectMapper.writeValueAsString(book));
+        return ResponseEntity.ok(book); // Đảm bảo trả về ResponseEntity
     }
 
     @PostMapping("")
