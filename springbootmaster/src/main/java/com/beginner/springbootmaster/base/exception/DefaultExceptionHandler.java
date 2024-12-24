e04468c04d4d6a70c7e6bcc62664e00ba7411df4
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-24 18:22:46 +0700

    valid exception

--- springbootmaster/src/main/java/com/beginner/springbootmaster/base/exception/DefaultExceptionHandler.java
+++ springbootmaster/src/main/java/com/beginner/springbootmaster/base/exception/DefaultExceptionHandler.java
@@ -1,8 +1,10 @@
 package com.beginner.springbootmaster.base.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -40,6 +42,24 @@ public class DefaultExceptionHandler {
         );
 
         return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiError> handleException(
+            MethodArgumentNotValidException e,
+            HttpServletRequest request) {
+        List<String> errors = e.getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                ZonedDateTime.now(),
+                errors
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
