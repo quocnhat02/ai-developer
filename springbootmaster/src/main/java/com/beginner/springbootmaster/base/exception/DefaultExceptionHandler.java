db24abdc05c4517df6520498a54f70f450499a04
lightcodese <nguyenquocnhat12b1@gmail.com>
2024-12-24 18:03:13 +0700

    controlleradvise

--- springbootmaster/src/main/java/com/beginner/springbootmaster/base/exception/DefaultExceptionHandler.java
+++ springbootmaster/src/main/java/com/beginner/springbootmaster/base/exception/DefaultExceptionHandler.java
@@ -3,11 +3,16 @@ package com.beginner.springbootmaster.base.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.ZonedDateTime;
 import java.util.List;
 
+@ControllerAdvice
 public class DefaultExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiError> handleException(
             ResourceNotFoundException e,
             HttpServletRequest request) {
@@ -20,5 +25,20 @@ public class DefaultExceptionHandler {
         );
 
         return new ResponseEntity<>(apiError, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiError> handleException(
+            Exception e,
+            HttpServletRequest request) {
+        ApiError apiError = new ApiError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ZonedDateTime.now(),
+                List.of()
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
